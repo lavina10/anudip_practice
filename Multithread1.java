25e4b6878619a076a966628c7505a6edc67e8750
lavina10 <80336350+lavina10@users.noreply.github.com>
2021-12-10 22:56:37 +0530

    Add files via upload

---
+++ Multithread1.java
@@ -0,0 +1,17 @@
+package prac;
+
+public class Multithread1 extends Thread {
+	public void run()  
+    {  
+      try {  
+          System.out.println("thread is executing now........");  
+      } catch(Exception e) {  
+      }   
+    }  
+    public static void main (String[] args) {  
+        Multithread1 m1= new Multithread1();  
+        m1.start();  
+        m1.start();  
+    }  
+
+}
