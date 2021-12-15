181499775b66dfe546bb94b330eed92c778defb6
lavina10 <80336350+lavina10@users.noreply.github.com>
2021-12-15 23:07:52 +0530

    Create Name.java

---
+++ assignment9(dec14)/Name.java
@@ -0,0 +1,11 @@
+package prac;
+
+public class Name {
+	public void display() {
+		 System.out.println("Lavina Gaykar");
+		 }
+		 public static void main(String[] args) {
+		 Name myname = new Name(); 
+		 myname.display(); 
+		 }
+}
